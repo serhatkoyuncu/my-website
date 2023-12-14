8becd43fcc823cf64b7ee55dbc4dd757956914bc
serhatkoyuncu <serhat_koyuncu@icloud.com>
2023-12-14 19:21:53 +0300

    fixes

---
+++ blocks/banner/banner.js
@@ -0,0 +1,37 @@
+export default function decorate(block) {
+  //   const cols = [...block.firstElementChild.children];
+  //   block.classList.add(`banner-${cols.length}-cols`);
+
+  // setup image banner
+  [...block.children].forEach((row) => {
+    [...row.children].forEach((col) => {
+      const a = col.querySelectorAll("a");
+      const pic = col.querySelector("picture");
+      const br = document.createElement("br");
+      const h3 = col.querySelectorAll("h3");
+      const newH3 = document.createElement("h3");
+      if (a.length) {
+        a.forEach((item) => {
+          item.setAttribute("target", "_blank");
+        });
+      }
+      if (h3.length) {
+        h3.forEach((item) => {
+          newH3.append(br);
+          newH3.append(item.textContent);
+          item.remove();
+        });
+        col.prepend(newH3);
+      }
+      if (pic) {
+        const picWrapper = pic.closest("div");
+        if (picWrapper && picWrapper.children.length === 1) {
+          // picture is only content in column
+          picWrapper.classList.add("banner-img");
+        } else {
+          picWrapper.classList.add("banner-inline-img");
+        }
+      }
+    });
+  });
+}
