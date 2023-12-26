fadf050b3f1363cc6cf5be284d6ceb75c99bc9be
serhatkoyuncu <serhat_koyuncu@icloud.com>
2023-12-26 17:02:44 +0300

    projects fix

--- blocks/projects/projects.js
+++ blocks/projects/projects.js
@@ -13,13 +13,37 @@ export default function decorate(block) {
     });
     ul.append(li);
   });
-  ul.querySelectorAll("img").forEach((img) =>
+
+  const playVideo = (item) => {
+    item.target.closest("div").querySelector("video").style.display = "block";
+    item.target.closest("div").querySelector("video").play();
+    item.target.closest("div").querySelector("picture").style.display = "none";
+  };
+  const pauseVideo = (item) => {
+    item.target.closest("div").querySelector("video").pause();
+    item.target.closest("div").querySelector("video").style.display = "none";
+    item.target.closest("div").querySelector("picture").style.display = "block";
+  };
+
+  ul.querySelectorAll("img").forEach((img, index) => {
     img
       .closest("picture")
       .replaceWith(
         createOptimizedPicture(img.src, img.alt, false, [{ width: "750" }])
-      )
-  );
+      );
+  });
+  ul.querySelectorAll(".projects-card-image").forEach((item, index) => {
+    const video = document.createElement("video");
+    const source = document.createElement("source");
+    video.id = `video-${index + 1}`;
+    video.controls = true;
+    source.src = `../../assets/video-${index + 1}.mov`;
+    source.type = "video/mp4";
+    video.append(source);
+    item.append(video);
+    item.addEventListener("mouseover", playVideo);
+    item.addEventListener("mouseleave", pauseVideo);
+  });
   block.textContent = "";
   block.append(ul);
 }
